57fe0dc9f21cd6f29461ec09a054786071d36bd4
Jeff Norton <jeff12ntn@gmail.com>
2016-07-05 14:24:44 -0600

    Custom Table View Cell phase completed

---
+++ Alarm/SwitchTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Jeff Norton on 7/5/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    
+    // MARK: - Stored Properties
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    
+    // MARK: - UITableViewCell
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func switchValueChanged(sender: UISwitch) {
+    }
+    
+
+}
